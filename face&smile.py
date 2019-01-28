f33b57c624388002a0fc718ddf7fdf6cda36e6e1
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-01-28 21:47:16 +0530

    Update face&smile.py

--- face&smile.py
+++ face&smile.py
@@ -1,34 +1,39 @@
-import cv2
+import cv2 as cv
 import pandas as pd
 from _datetime import datetime
 Id = input('Enter Id')
-face_classifier = cv2.CascadeClassifier('cascade.xml')
-smile_classifier = cv2.CascadeClassifier('Haar_smile.xml')
+face_classifier = cv.CascadeClassifier('cascade.xml')
+smile_classifier = cv.CascadeClassifier('Haar_smile.xml')
 times = []
 smile_ratios = []
-cap = cv2.VideoCapture(0)
+sm_ratio = 0
+cap = cv.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (21, 21), 0)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
         roi_gray = gray[y:y + h, x:x + w]
         roi_img = img[y:y + h, x:x + w]
         smile = smile_classifier.detectMultiScale(roi_gray, scaleFactor=1.2,
                                                   minNeighbors=22,
                                                   minSize=(25, 25))
+        print("Found {0} faces!".format(len(faces)))
+
         for (sx, sy, sw, sh) in smile:
-            cv2.rectangle(roi_img, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 1)
+            cv.rectangle(roi_img, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 1)
+            print(sw)
+            print(sx)
             sm_ratio = str(round(sw / sx, 3))
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(img, 'Smile meter : ' + sm_ratio, (10, 50), font, 1, (200, 255, 155), 2, cv2.LINE_AA)
+            font = cv.FONT_HERSHEY_SIMPLEX
+            cv.putText(img, 'Smile meter : ' + sm_ratio, (10, 50), font, 1, (200, 255, 155), 2, cv.LINE_AA)
             if float(sm_ratio) > 1.8:
-                cv2.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img)
-    cv2.imshow('Smile Detector', img)
-    if cv2.waitKey(1) & 0xFF == ord(' '):
+                cv.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img)
+    cv.imshow('Smile Detector', img)
+    if cv.waitKey(1) & 0xFF == ord(' '):
         break
 smile_ratios.append(float(sm_ratio))
 times.append(datetime.now())
@@ -38,4 +43,4 @@ with open('smile_records.csv', 'a') as csvFile:
     df.to_csv(csvFile, header=False, index=False)
 csvFile.close()
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
