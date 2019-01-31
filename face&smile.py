1c37fc1f383f37870ea2db60e832a8b6e1a92b3e
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-01-31 23:13:24 +0530

    Update face&smile.py

--- face&smile.py
+++ face&smile.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import pandas as pd
 from _datetime import datetime
-Id = input('Enter Id')
+Id = input('Enter Id:-')
 face_classifier = cv.CascadeClassifier('cascade.xml')
 smile_classifier = cv.CascadeClassifier('Haar_smile.xml')
 times = []
@@ -9,8 +9,9 @@ smile_ratios = []
 sm_ratio = 0
 cap = cv.VideoCapture(0)
 
-while 1:
+while True:
     ret, img = cap.read()
+    ret2, img2 = cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = cv.GaussianBlur(gray, (21, 21), 0)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
@@ -21,23 +22,21 @@ while 1:
         smile = smile_classifier.detectMultiScale(roi_gray, scaleFactor=1.2,
                                                   minNeighbors=22,
                                                   minSize=(25, 25))
-        print("Found {0} faces!".format(len(faces)))
+       # print("Found {0} faces!".format(len(faces)))
 
         for (sx, sy, sw, sh) in smile:
             cv.rectangle(roi_img, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 1)
-            print(sw)
-            print(sx)
-            sm_ratio = str(round(sw / sx, 3))
+            sm_ratio = str(round(sw / sx, 2))
             font = cv.FONT_HERSHEY_SIMPLEX
             cv.putText(img, 'Smile meter : ' + sm_ratio, (10, 50), font, 1, (200, 255, 155), 2, cv.LINE_AA)
             if float(sm_ratio) > 1.8:
-                cv.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img)
+                cv.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img2)
     cv.imshow('Smile Detector', img)
     if cv.waitKey(1) & 0xFF == ord(' '):
         break
 smile_ratios.append(float(sm_ratio))
 times.append(datetime.now())
-ds = {'ID': Id, 'smile_ratio': smile_ratios, 'times': times}
+ds = {'ID': Id, 'Smile Score ': smile_ratios, 'Times': times}
 df = pd.DataFrame(ds)
 with open('smile_records.csv', 'a') as csvFile:
     df.to_csv(csvFile, header=False, index=False)
