6001c24b062972ec3ec426f231dfcad7d03c5862
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-02-02 10:04:44 +0530

    Update face&smile.py

--- face&smile.py
+++ face&smile.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import pandas as pd
+import time
 from _datetime import datetime
 Id = input('Enter Id:-')
 face_classifier = cv.CascadeClassifier('cascade.xml')
@@ -7,11 +8,12 @@ smile_classifier = cv.CascadeClassifier('Haar_smile.xml')
 times = []
 smile_ratios = []
 sm_ratio = 0
+sm_try = 0
 cap = cv.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
-    ret2, img2 = cap.read()
+   # ret2, img2 = cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = cv.GaussianBlur(gray, (21, 21), 0)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
@@ -29,12 +31,15 @@ while True:
             sm_ratio = str(round(sw / sx, 2))
             font = cv.FONT_HERSHEY_SIMPLEX
             cv.putText(img, 'Smile meter : ' + sm_ratio, (10, 50), font, 1, (200, 255, 155), 2, cv.LINE_AA)
-            if float(sm_ratio) > 1.8:
-                cv.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img2)
+            if float(sm_ratio) > 3.8:
+                sm_try = sm_ratio
+                cv.imwrite('C:/Users/My Lappy/Pictures/Saved Pictures/'+Id+'.jpg', img)
+
     cv.imshow('Smile Detector', img)
+    #time.sleep(10)
     if cv.waitKey(1) & 0xFF == ord(' '):
         break
-smile_ratios.append(float(sm_ratio))
+smile_ratios.append(float(sm_try))
 times.append(datetime.now())
 ds = {'ID': Id, 'Smile Score ': smile_ratios, 'Times': times}
 df = pd.DataFrame(ds)
