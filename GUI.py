15d28b4dfc08efd733e60f203dc82716f88cc663
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-02-07 23:41:29 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,17 +1,20 @@
 from tkinter import *
-from PIL import Image
-from PIL import ImageTk
-from BarScannerCam import sc
-root = Tk()
-#one = Label(root, text="One", bg="red", fg="white")
-#C = Canvas(root, bg="blue", height=250, width=300)
-filename = PhotoImage(file="Persistent_logo.png")
-filename = filename.zoom(3)
-background_label = Label(root, image=filename)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
-b = Button(root, text="RUN", command=sc)
-b.pack()
-#C.pack()
-background_label.pack()
-
-root.mainloop()
+from BarScannerCam import Bar
+class GUI(Bar):
+    def ui(self):
+        #global obj, GUI
+        root = Tk()
+        # one = Label(root, text="One", bg="red", fg="white")
+        # C = Canvas(root, bg="blue", height=250, width=300)
+        filename = PhotoImage(file="Persistent_logo.png")
+        filename = filename.zoom(2)
+        background_label = Label(root, image=filename)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        b = Button(root, text="RUN", command=obj.sc)
+        b.pack()
+        # C.pack()
+        background_label.pack()
+        root.mainloop()
+obj = GUI()
+obj.ui()
+#print(Bar.sc.text)
