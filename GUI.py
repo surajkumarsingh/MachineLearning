c946d640bc1e0368803226ff933b182beacd9607
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-02-08 17:37:04 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,20 +1,29 @@
 from tkinter import *
-from BarScannerCam import Bar
-class GUI(Bar):
+from QRScannerAndSmile import QRScannerAndSmile
+
+
+class GUI(QRScannerAndSmile):
+
     def ui(self):
-        #global obj, GUI
         root = Tk()
         # one = Label(root, text="One", bg="red", fg="white")
         # C = Canvas(root, bg="blue", height=250, width=300)
         filename = PhotoImage(file="Persistent_logo.png")
-        filename = filename.zoom(2)
-        background_label = Label(root, image=filename)
-        background_label.place(x=0, y=0, relwidth=1, relheight=1)
-        b = Button(root, text="RUN", command=obj.sc)
+        filename = filename.zoom(1)
+        # background_label = Label(root, image=filename)
+        # background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        b = Button(root, activebackground="Green", bd=4, text="RUN", image=filename, command=obj.call)
         b.pack()
         # C.pack()
-        background_label.pack()
+        # background_label.pack()
         root.mainloop()
+
+
 obj = GUI()
 obj.ui()
-#print(Bar.sc.text)
+
+
+
+
+
+
