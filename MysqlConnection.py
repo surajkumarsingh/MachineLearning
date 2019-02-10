7c4457f0f99665a171cbb70eb7f80767b54db6fc
surajkumarsingh <43388412+surajkumarsingh@users.noreply.github.com>
2019-02-10 22:29:36 +0530

    Update MysqlConnection.py

--- MysqlConnection.py
+++ MysqlConnection.py
@@ -1,10 +1,49 @@
 import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(host="localhost", user="root", password="root")
 
-mycursor = mydb.cursor()
+def convertToBinaryData(filename):
+    #Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
 
-mycursor.execute("show databases")
 
-for i in mycursor:
-    print(i)
+def insertdata(id, name, dt, image):
+    print("Inserting Data")
+
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                             database ='suraj',
+                             user ='root',
+                             password ='root')
+
+        cursor = connection.cursor(prepared=True)
+
+        sql_insert_blob_query = """ INSERT INTO `test`
+                          (`id`, `name`, `dt`, `image`) VALUES (%s,%s,%s,%s)"""
+
+        empPicture = convertToBinaryData(image)
+
+
+        # Convert data into tuple format
+        insert_blob_tuple = (id, name, dt, empPicture)
+
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        connection.commit()
+        print("Image and file inserted successfully as a BLOB into python_employee table", result)
+
+    except mysql.connector.Error as error:
+        connection.rollback()
+        print("Failed inserting BLOB data into MySQL table {}".format(error))
+
+    finally:
+        #closing database connection.
+        if(connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+#insertBLOB(78999, "ghkll", "2019-02-09 17:28:32", "C:\\Users\My Lappy\Pictures\Jim.jpg")
